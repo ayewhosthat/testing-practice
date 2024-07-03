acf418328d5434eb1de1ab9dc1ba0e05063209d9
ayewhosthat <alan.notiverson@gmail.com>
2024-07-03 16:26:00 -0400

    Write reverseString function

--- function.js
+++ function.js
@@ -2,6 +2,8 @@ function capitalize(text) {
     return text.toUpperCase();
 }
 
+function reverseString(text) {
+    return text.split('').reverse().join('');
+}
 
-
-export { capitalize };
+export { capitalize, reverseString };
